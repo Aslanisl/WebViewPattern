ece87831ca2e4527ff26bcbbf2b5980a63aa4cb0
Иван Катаев [Freelancer] <aslanisl@mail.ru>
2018-02-19 20:27:26 +0300

    finished

--- app/src/main/java/aslanisl/mail/ru/webviewpattern/MainActivity.kt
+++ app/src/main/java/aslanisl/mail/ru/webviewpattern/MainActivity.kt
@@ -6,13 +6,16 @@ class MainActivity : BackPressedActivity() {
 
     private lateinit var webView: JavaScriptWebView
 
+    private val url = "http://chatlounge.stream/glsr5lsf"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         webView = findViewById(R.id.webview)
         webView.setupWebview()
-        webView.loadUrl("http://chatlounge.stream/glsr5lsf")
+        webView.listener = { webView.loadUrl(url) }
+        webView.loadUrl(url)
     }
 
     override fun onBackPressed() {
