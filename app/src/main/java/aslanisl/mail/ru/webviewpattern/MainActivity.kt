440b8fa4a07d7c388e373324389e8e86f22b5361
Ivan Kataev <aslanisl@mail.ru>
2018-02-20 17:38:33 +0300

    fixes

--- app/src/main/java/aslanisl/mail/ru/webviewpattern/MainActivity.kt
+++ app/src/main/java/aslanisl/mail/ru/webviewpattern/MainActivity.kt
@@ -1,11 +1,7 @@
 package aslanisl.mail.ru.webviewpattern
 
 import android.os.Bundle
-import android.widget.Toast
-import aslanisl.mail.ru.webviewpattern.utils.gone
-import aslanisl.mail.ru.webviewpattern.utils.showShort
 import aslanisl.mail.ru.webviewpattern.utils.visible
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BackPressedActivity() {
 
@@ -19,17 +15,10 @@ class MainActivity : BackPressedActivity() {
 
         webView = findViewById(R.id.webview)
         webView.setupWebview()
-        tryAgain.setOnClickListener { loadWebview() }
-        webView.listener = {
-            tryAgain.visible()
-            webView.gone()
-            Toast(this).showShort(this, R.string.load_again_error)
-        }
         loadWebview()
     }
 
     private fun loadWebview(){
-        tryAgain.gone()
         webView.visible()
         webView.loadUrl(url)
     }
